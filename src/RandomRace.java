f9936190b5e39fb61ce8055e9b9903653725d048
Janith Chanaka Abhayarathna <abhayaratnajanith3@gmail.com>
2021-11-24 21:21:20 +0530

    Final changes for the day.

--- src/RandomRace.java
+++ src/RandomRace.java
@@ -6,7 +6,7 @@ import java.util.Collections;
 
 public class RandomRace {
 
-    private String[] newPositions = new String[20];
+    private String[] newPositions = new String[Formula1ChampionshipManager.drivers.size()];
     public JTable randomTable = new JTable();
     public static ArrayList<RandomRace> rRace = new ArrayList<RandomRace>();
 
@@ -39,20 +39,21 @@ public class RandomRace {
 
         for (int i = 0; i < Formula1ChampionshipManager.drivers.size(); i++) {
             Collections.shuffle(Formula1ChampionshipManager.drivers);
-            newPositions[i] = Formula1ChampionshipManager.drivers.get(i).getDriverName();
-            Formula1ChampionshipManager.drivers.get(i).setNumOfRaces(1);
-            Formula1ChampionshipManager.drivers.get(i).setPoints(Formula1ChampionshipManager.pointsScheme[i]);
-            if (i == 0) {
-                Formula1ChampionshipManager.drivers.get(i).setPosition1(1);
-            } else if (i == 1) {
-                Formula1ChampionshipManager.drivers.get(i).setPosition2(1);
-            } else if (i == 2) {
-                Formula1ChampionshipManager.drivers.get(i).setPosition3(1);
+            if (randomPositionValidate(newPositions)) {
+                newPositions[i] = Formula1ChampionshipManager.drivers.get(i).getDriverName();
+                Formula1ChampionshipManager.drivers.get(i).setNumOfRaces(1);
+                Formula1ChampionshipManager.drivers.get(i).setPoints(Formula1ChampionshipManager.pointsScheme[i]);
+                if (i == 0) {
+                    Formula1ChampionshipManager.drivers.get(i).setPosition1(1);
+                } else if (i == 1) {
+                    Formula1ChampionshipManager.drivers.get(i).setPosition2(1);
+                } else if (i == 2) {
+                    Formula1ChampionshipManager.drivers.get(i).setPosition3(1);
+                }
             }
         }
 
         Formula1ChampionshipManager.races.add(new RaceData(date, newPositions));
-
 
         JFrame frame = new JFrame(" Formula 1 Championship Manager Program.");
         frame.getContentPane().setBackground(Color.black);
@@ -97,4 +98,20 @@ public class RandomRace {
     public void setRandomDate(String randomDate) {
         this.randomDate = randomDate;
     }
+
+    public boolean randomPositionValidate(String[] newPositions) {
+
+        if (newPositions.length > 0) {
+            for (int i = 0; i < Formula1ChampionshipManager.drivers.size(); i++) {
+                for (int f = 0; f < newPositions.length; f++) {
+                    if (Formula1ChampionshipManager.drivers.get(i).getDriverName().equals(newPositions[f])) {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+        return true;
+    }
+
 }
